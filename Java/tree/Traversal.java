4618d7fb4ecb3acc47f511d215a0e9b57fc63577
Peng Zhao <clairzhang2019@gmail.com>
2020-03-21 15:43:13 -0700

    adding another implementation for preorder traversal, same framework as inorder

--- Java/tree/Traversal.java
+++ Java/tree/Traversal.java
@@ -15,7 +15,7 @@ public class Traversal {
                 root = root.left;
             } else {
                 root = treeNodes.pop();
-                result.add(root.val);
+                result.add(root.val); // only different from preorder
                 root = root.right;
             }
         }
@@ -23,6 +23,24 @@ public class Traversal {
         return result;
     }
 
+    public List<Integer> preorderTraversal2 (TreeNode root) {
+        Stack<TreeNode> treeNodes = new Stack<TreeNode>();
+        List<Integer> list = new ArrayList<Integer>();
+        
+        while (!treeNodes.isEmpty() || root != null) {
+            if (root != null) {
+                list.add(root.val); // only difference from inorder
+                treeNodes.push(root);
+                root = root.left;
+            } else {
+                root = treeNodes.pop();
+                root = root.right;
+            }
+        }
+        
+        return list;
+    }
+    
     public List<Integer> preorderTraversal(TreeNode root) {
         Stack<TreeNode> treeNodes = new Stack<TreeNode>();
         List<Integer> result = new ArrayList<Integer>();
